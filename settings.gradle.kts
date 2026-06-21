6b88a074f88419efc4718d3da1ca545ab31fde12
me <me@local.host>
2026-06-21 09:52:50 +0700

    a

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.pkg.github.com/rovo89/XposedBridge")
     }
 }
 
