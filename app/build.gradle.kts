ff958fa945124431ed1d5de5bb9dd27137399666
me <me@local.host>
2026-06-21 10:22:45 +0700

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,6 @@ android {
 
 dependencies {
     compileOnly("androidx.annotation:annotation:1.9.1")
-    compileOnly("de.robv.android.xposed:api:82")
-    compileOnly("io.github.libxposed:api:latest.release")
-    compileOnly("io.github.lsposed.xposed:api:8.0.0")
-    annotationProcessor("io.github.lsposed.xposed:api:8.0.0")
-    annotationProcessor("org.lsposed.exposed:exposed-api:0.10.0")
-    compileOnly("org.lsposed.exposed:exposed-api:0.10.0")
+    compileOnly("io.github.libxposed:api:101.0.0")
 }
+
