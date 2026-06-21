5768d3a024d172d776da15ae08f95a2e6ddc8886
me <me@local.host>
2026-06-21 10:38:59 +0700

    a

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,21 @@
-pluginManagement {
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+ pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jcenter.bintray.com") }
     }
 }
-
 rootProject.name = "XposedModuleTemplate"
 include(":app")
+
